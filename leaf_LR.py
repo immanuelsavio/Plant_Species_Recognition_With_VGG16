bc6e310a473d00d1cdcc62f63e4cf3329554f72d
root <immanuelsavio@gmail.com>
2018-07-25 15:30:37 +0530

    added code for user variables

--- leaf_LR.py
+++ leaf_LR.py
@@ -16,3 +16,18 @@ import matplotlib.pyplot as plt
 # load the user configs
 with open('conf_vgg16/flower_lr.json') as f:    
 	config = json.load(f)
+# config variables
+test_size = config["test_size"]
+seed = config["seed"]
+features_path = config["features_path"]
+labels_path = config["labels_path"]
+results = config["results"]
+classifier_path = config["classifier_path"]
+train_path = config["train_path"]
+num_classes = config["num_classes"]
+
+# import features and labels
+h5f_data = h5py.File(features_path, 'r')
+h5f_label = h5py.File(labels_path, 'r')
+seed= 9
+seed_value=np.random.seed(seed)
