eb9ea8770bf75470f8a90c0cb2b68466223b9212
root <immanuelsavio@gmail.com>
2018-07-26 10:31:15 +0530

    changed the format and extracting function

--- extract.py
+++ extract.py
@@ -1,16 +1,14 @@
 # filter warnings
 import warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
-
 # keras imports
-from keras.applications.xception import Xception, preprocess_input
+from keras.layers.core import Dense, Activation, Dropout
+from keras.applications.densenet import DenseNet121, preprocess_input
 from keras.preprocessing import image
-from sklearn.preprocessing import LabelEncoder
 from keras.models import Model
 from keras.models import model_from_json
-import pickle
-#from theano.tensor import blas
-#from theano.tensor.nnet import opt
+from keras.layers import Input
+from keras import *
 
 # other imports
 from sklearn.preprocessing import LabelEncoder
@@ -22,15 +20,12 @@ import os
 import json
 import datetime
 import time
-
-#setting the backend
 from keras import backend as K
-
 K.set_image_dim_ordering('tf')
-with open('conf_xception/flower.json') as a:    
 
-# config variables
-  config = json.load(a)
+# load the user configs
+with open('conf_densenet/flower.json') as f:    
+  config = json.load(f)
 model_name    = config["model"]
 weights     = config["weights"]
 include_top   = config["include_top"]
@@ -39,3 +34,9 @@ features_path = config["features_path"]
 labels_path   = config["labels_path"]
 test_size   = config["test_size"]
 results     = config["results"]
+print ("[status]start time - {}" .format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+
+base_model = DenseNet121(include_top=include_top,weights=weights)
+model = Model(input=base_model.input, output=base_model.output)
+model.summary()
+
