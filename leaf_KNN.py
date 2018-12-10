2fef12955e5a22bf899e6420d198f1e7eae1636c
immanuelsavio <immanuelsavio@gmail.com>
2018-12-10 18:41:02 +0530

    K-Nearest Classifier Ready for deployment

--- leaf_KNN.py
+++ leaf_KNN.py
@@ -38,3 +38,108 @@ labels = np.array(labels_string)
 
 h5f_data.close()
 h5f_label.close()
+
+# verify the shape of features and labels
+print "[INFO] features shape: {}".format(features.shape)
+print "[INFO] labels shape: {}".format(labels.shape)
+
+print "[INFO] training started..."
+# split the training and testing data
+(trainData, testData, trainLabels, testLabels) = train_test_split(np.array(features),
+                                                                  np.array(labels),
+                                                                  test_size=test_size,
+                                                                  random_state=seed_value)
+
+print "[INFO] splitted train and test data..."
+print "[INFO] train data  : {}".format(trainData.shape)
+print "[INFO] test data   : {}".format(testData.shape)
+print "[INFO] train labels: {}".format(trainLabels.shape)
+print "[INFO] test labels : {}".format(testLabels.shape)
+
+(trainData, valData, trainLabels, valLabels) = train_test_split(trainData, trainLabels,
+	test_size=0.1, random_state=seed_value)
+print("training data points: {}".format(len(trainLabels)))
+print("validation data points: {}".format(len(valLabels)))
+print("testing data points: {}".format(len(testLabels)))
+
+# initialize the values of k for our k-Nearest Neighbor classifier along with the
+# list of accuracies for each value of k
+kVals = range(1, 50, 2)
+accuracies = []
+
+# loop over various values of `k` for the k-Nearest Neighbor classifier
+for k in xrange(1, 50, 2):
+	# train the k-Nearest Neighbor classifier with the current value of `k`
+	model = KNeighborsClassifier(n_neighbors=k)
+	model.fit(trainData, trainLabels)
+
+	# evaluate the model and update the accuracies list
+	score = model.score(valData, valLabels)
+	print("k=%d, accuracy=%.2f%%" % (k, score * 100))
+	accuracies.append(score)
+
+# find the value of k that has the largest accuracy
+i = np.argmax(accuracies)
+print("k=%d achieved highest accuracy of %.2f%% on validation data" % (kVals[i],
+	accuracies[i] * 100))
+
+# re-train our classifier using the best k value and predict the labels of the
+# test data
+model = KNeighborsClassifier(n_neighbors=kVals[i])
+model.fit(trainData, trainLabels)
+
+
+# use rank-1 and rank-5 predictions
+print("[INFO] evaluating model...")
+f = open(results, "w")
+rank_1 = 0
+rank_5 = 0
+
+# loop over test data
+for (label, features) in zip(testLabels, testData):
+	# predict the probability of each class label and
+	# take the top-5 class labels
+	predictions = model.predict_proba(np.atleast_2d(features))[0]
+	predictions = np.argsort(predictions)[::-1][:5]
+
+	# rank-1 prediction increment
+	if label == predictions[0]:
+		rank_1 += 1
+
+	# rank-5 prediction increment
+	if label in predictions:
+		rank_5 += 1
+
+# convert accuracies to percentages
+rank_1 = (rank_1 / float(len(testLabels))) * 100
+rank_5 = (rank_5 / float(len(testLabels))) * 100
+
+# write the accuracies to file
+f.write("Rank-1: {:.2f}%\n".format(rank_1))
+f.write("Rank-5: {:.2f}%\n\n".format(rank_5))
+
+# evaluate the model of test data
+preds = model.predict(testData)
+
+# write the classification report to file
+f.write("{}\n".format(classification_report(testLabels, preds)))
+f.close()
+
+# dump classifier to file
+print("[INFO] saving model...")
+f = open(classifier_path, "w")
+f.write(cPickle.dumps(model))
+f.close()
+
+# display the confusion matrix
+print ("[INFO] confusion matrix")
+
+# get the list of training lables
+labels = sorted(list(os.listdir(train_path)))
+
+# plot the confusion matrix
+cm = confusion_matrix(testLabels, preds)
+sns.heatmap(cm,
+            annot=True,
+            cmap="Set2")
+plt.show()
