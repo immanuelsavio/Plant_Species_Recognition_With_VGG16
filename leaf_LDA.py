df463d5ec7e98d283cdb535aa60e8d930e7b6966
_Algorithm_Kid98 <30970161+immanuelsavio@users.noreply.github.com>
2018-12-11 11:10:13 +0530

    half LDA Completed

---
+++ leaf_LDA.py
@@ -0,0 +1,40 @@
+#This is used to classify using the leniar decrement analysis algorithm
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import cPickle
+import h5py
+import os
+import json
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# load the user configs
+with open('conf_vgg16/flower_lda.json') as f:    
+	config = json.load(f)
+
+# config variables
+test_size = config["test_size"]
+seed = config["seed"]
+features_path = config["features_path"]
+labels_path = config["labels_path"]
+results = config["results"]
+classifier_path = config["classifier_path"]
+train_path = config["train_path"]
+num_classes = config["num_classes"]
+seed= 9
+seed_value=np.random.seed(seed)
+# import features and labels
+h5f_data = h5py.File(features_path, 'r')
+h5f_label = h5py.File(labels_path, 'r')
+
+features_string = h5f_data['dataset_1']
+labels_string = h5f_label['dataset_1']
+
+features = np.array(features_string)
+labels = np.array(labels_string)
+
+h5f_data.close()
+h5f_label.close()
